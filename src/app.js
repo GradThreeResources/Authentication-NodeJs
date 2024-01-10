a32a2c1bcf743218a8b8d5737e7b36baeeebd31a
MohamedDiaaEldin <mdiaan404@gmail.com>
2024-01-10 09:59:02 +0200

    update routes order

--- src/app.js
+++ src/app.js
@@ -10,12 +10,12 @@ app.use(bodyParser.json());
 
 // for development
 app.use(morgan('dev'))
-app.use('/', (req, res)=>{
-    res.send('<h1>Testing...</h1>')
-})
 // User related routes
 app.use(userRouter)
 
+app.use('/', (req, res)=>{
+    res.send('<h1>Testing...</h1>')
+})
 
 syncModels()
 app.listen(process.env.PORT || 1000,()=>console.log('Server is listing')) ;
