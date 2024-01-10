a3fd31d9b26a94ea7f24923832f5a42ab60b327b
MohamedDiaaEldin <mdiaan404@gmail.com>
2024-01-10 08:36:07 +0200

    update database connection

--- src/database-config.js
+++ src/database-config.js
@@ -1,22 +1,34 @@
 const { Sequelize } = require("sequelize");
-require('dotenv').config();
+require("dotenv").config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, 'postgres', {
-    host: process.env.DB_HOST,
-    dialect: 'postgres'
+// dev
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, 'postgres', {
+//     host: process.env.DB_HOST,
+//     dialect: 'postgres',
+// });
+
+const databaseUrl = process.env.DATABASE_URL;
+
+// Initialize Sequelize with the database URL
+const sequelize = new Sequelize(databaseUrl, {
+  dialect: "postgres", // Specify the dialect (postgres in this case)
+  // Other options if required: logging: false, ssl: true (for SSL connections), etc.
+  dialectOptions: {
+    ssl: {
+      require: true,
+    },
+  },
 });
 
+module.exports.syncModels = async () => {
+  try {
+    // Sync all defined models with the database
+    await sequelize.sync({ force: false });
+    // { force: true } will drop existing tables and recreate them; use with caution in production
 
-module.exports.syncModels = async ()=> {
-    try {
-      // Sync all defined models with the database
-      await sequelize.sync({force:false});
-      // { force: true } will drop existing tables and recreate them; use with caution in production
-  
-      console.log('All models were synchronized successfully.');
-
-    } catch (error) {
-      console.error('Error synchronizing models:', error);
-    }
+    console.log("All models were synchronized successfully.");
+  } catch (error) {
+    console.error("Error synchronizing models:", error);
   }
-module.exports.sequelize = sequelize
+};
+module.exports.sequelize = sequelize;
