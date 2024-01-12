2242593e62fa2405d1ccc42967da2be75b2e2f3f
MohamedDiaaEldin <mdiaan404@gmail.com>
2024-01-12 09:21:06 +0200

    change to database production url

--- src/database-config.js
+++ src/database-config.js
@@ -2,18 +2,18 @@ const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
 // dev
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, 'postgres', {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-    logging:false
-});
-
-// const databaseUrl = process.env.DATABASE_URL;
-
-// // Initialize Sequelize with the database URL
-// const sequelize = new Sequelize(databaseUrl, {
-//   dialect: "postgres", // Specify the dialect (postgres in this case)    
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, 'postgres', {
+//     host: process.env.DB_HOST,
+//     dialect: 'postgres',
+//     logging:false
 // });
+
+const databaseUrl = process.env.DATABASE_URL;
+
+// Initialize Sequelize with the database URL
+const sequelize = new Sequelize(databaseUrl, {
+  dialect: "postgres", // Specify the dialect (postgres in this case)    
+});
 
 module.exports.syncModels = async () => {
   try {
